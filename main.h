f29b2fca80116adec5be62a53c49f62b141dd729
root <root@DESKTOP-VS17LS8.localdomain>
2024-12-16 18:41:21 +0100

    feat: add header file

--- main.h
+++ main.h
@@ -1,4 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+int path_finder(char *cmd, char *work_buffer);
+int file_checker(char *file);
+int execute_cmd(char *argv[]);
+char **token_args(char *input_buffer);
+void print_env(void);
+int _error(void);
+char *_getenv(const char *name);
+
 #endif
