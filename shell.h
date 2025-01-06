1ddbfe50515d8cbc30effd0820cf3957b6a61408
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-01-06 16:10:34 +0100

    update the library

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+int _error(void);
+
+
+#endif
