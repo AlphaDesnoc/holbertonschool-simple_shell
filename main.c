c9064f6d4ebafed03b259569039653c3bb310062
root <root@DESKTOP-VS17LS8.localdomain>
2025-01-09 16:13:09 +0100

    Task Modify for no interactive mode

--- main.c
+++ main.c
@@ -42,25 +42,22 @@ int main(void)
 
 	signal(SIGINT, SIG_IGN);
 	do {
-		cwd = getcwd(NULL, 0);
-		if (cwd != NULL)
+		if (isatty(STDIN_FILENO))
 		{
-			printf("\033[36m%s\033[00m$ ", cwd);
-			free(cwd);
+			cwd = getcwd(NULL, 0);
+			if (cwd != NULL)
+			{
+				printf("\033[36m%s\033[00m$ ", cwd);
+				free(cwd);
+			}
+			else
+				perror("getcwd() error");
 		}
-		else
-			perror("getcwd() error");
 		fflush(stdin);
 		char_read = getline(&input_buffer, &size_allocated, stdin);
-		if (char_read <= 1)
-			continue;
-		if (char_read == EOF)
-		{
-			free(input_buffer);
-			if (isatty(STDIN_FILENO))
-				putchar('\n');
-			return (ret);
-		}
+		if (char_read <= 0)
+			break;
+
 		input_buffer[char_read - 1] = '\0';
 		if (strncmp(input_buffer, "env", 3) == 0)
 		{
@@ -73,8 +70,7 @@ int main(void)
 		if (myargv[0] != NULL)
 			ret = execute_cmd(myargv);
 		free(myargv);
-	} while (1);
+	} while (isatty(STDIN_FILENO));
 	free(input_buffer);
 	return (ret);
 }
-
